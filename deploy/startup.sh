7ea4af0fc0dc769f03c1892eec67c039f70a0572
lnsoftware <lnsoftware@163.com>
2021-08-29 10:03:53 +0000

    update deploy/startup.sh.

--- deploy/startup.sh
+++ deploy/startup.sh
@@ -4,6 +4,6 @@ image_name=weather
 node_user=ubuntu
 node_ip=110.42.143.198
 
-ssh $node_user@$node_ip "docker login --username=$registry_user --password=$registry_pass"
-ssh $node_user@$node_ip "docker pull $image_name && docker rm -f $image_name || true "
-ssh $node_user@$node_ip "docker run -itd --name=$image_name --restart=always  --net=host  -e TZ="Asia/Shanghai" $image_name"
+ssh $node_user@$node_ip "sudo docker login --username=$registry_user --password=$registry_pass"
+ssh $node_user@$node_ip "sudo docker pull $image_name && docker rm -f $image_name || true "
+ssh $node_user@$node_ip "sudo docker run -itd --name=$image_name --restart=always  --net=host  -e TZ="Asia/Shanghai" $image_name"

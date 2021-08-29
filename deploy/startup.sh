422b6eaf5a90c2ff1b44d2e20cf8490497183c53
lnsoftware <lnsoftware@163.com>
2021-08-29 08:32:40 +0000

    update deploy/startup.sh.

--- deploy/startup.sh
+++ deploy/startup.sh
@@ -1,8 +1,8 @@
 registry_user=$1
 registry_pass=$2
-$image_name=weather
-$node_user=ubuntu
-$node_ip=110.42.143.198
+image_name=weather
+node_user=ubuntu
+node_ip=110.42.143.198
 
 ssh $node_user@$node_ip "docker login --username=$registry_user --password=$registry_pass"
 ssh $node_user@$node_ip "docker pull $image_name && docker rm -f $image_name || true "
